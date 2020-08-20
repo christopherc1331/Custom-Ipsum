cb4b5db6e2ae984cc2e5ac18ae28131ffdffd8b5
Chris Cruz <christopherc1331@gmail.com>
2020-08-19 22:31:42 -0700

    Basic styling established

--- src/App.js
+++ src/App.js
@@ -9,19 +9,45 @@ function App() {
 
   return (
     <Container>
-      <Paragraphs>{inputText}</Paragraphs>
+      <HeaderContainer>
+        <h1>Custom Ipsum</h1>
+        <h3>Create gibberish that makes sense to you!</h3>
+      </HeaderContainer>
+      <BodyContainer>
+        <Paragraphs>{inputText}</Paragraphs>
+      </BodyContainer>
     </Container>
   );
 }
 
 const Container = styled.div`
-  height: 100vh;
-  width: 100%;
   margin: 0 auto;
+  max-height: 100vh;
+  overflow-y: hidden;
+`;
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  min-height: 3rem;
+  margin-bottom: 1.5rem;
+  padding-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  /* border: 1px solid black; */
+  background-color: #414972;
+  h1,
+  h3 {
+    color: #a2ace8;
+  }
+`;
+
+const BodyContainer = styled.div`
+  padding: 0 1.5rem;
 `;
 
 const Paragraphs = styled.p`
   white-space: pre-line;
+  color: #735d4e;
 `;
 
 export default App;
